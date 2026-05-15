c89f804e08c8930e27b297e2920a71d2ab64f144
limon-daohaosisi <2235927772@qq.com>
2026-05-16 00:36:39 +0800

    fix:cicd

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -7,9 +7,10 @@ plugins {
 android {
     namespace = "com.example.myapplication"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version =
+            release(36) {
+                minorApiLevel = 1
+            }
     }
 
     defaultConfig {
@@ -27,7 +28,7 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
     }
@@ -65,6 +66,6 @@ tasks.register("androidCheck") {
         "ktlintCheck",
         "lintDebug",
         "testDebugUnitTest",
-        "assembleDebug"
+        "assembleDebug",
     )
 }
